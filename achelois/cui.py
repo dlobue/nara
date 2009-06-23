1165fe80fc5eb37215d18db8a4af338d42b174d5
root <root@megaera.local>
2009-06-22 23:24:19 -0700

    implemented method passing to "child" classes. walker still does not detect changes though :(

--- achelois/cui.py
+++ achelois/cui.py
@@ -7,13 +7,31 @@ import lazythread
 import index
 
 #class convContainer(dict):
-class conv_repr(urwid.Text, dict):
+def _callback(fn):
+    def wrapper(self, *args):
+        r = fn(self, *args)
+        self.parentmethod()
+        return r
+    return wrapper
+
+class callback_list(list):
+    def __init__(self, parentmethod):
+        self.parentmethod = parentmethod
+
+    append = _callback(list.append)
+    extend = _callback(list.extend)
+    __setslice__ = _callback(list.__setslice__)
+    __setitem__ = _callback(list.__setitem__)
+    
+class conv_repr(lazythread.convContainer):
     def __init__(self, msgids, subjects, labels, messages):
-        self.__new__(urwid.Button)
+        self.widget = urwid.Text('Loading', wrap='clip')
+
         self['msgids'] = msgids
         self['subjects'] = subjects
-        self['messages'] = messages
         self['labels'] = labels
+        self['messages'] = callback_list(self.update_widget)
+        self['messages'].extend(messages)
         
         #becuse attributes are fun
         self.msgids = self['msgids']
@@ -21,12 +39,24 @@ class conv_repr(urwid.Text, dict):
         self.messages = self['messages']
         self.labels = self['labels']
 
-        self.user_data = None
-        self.on_press = None
-        self.set_wrap_mode('clip')
-        self.set_label(self.__repr__())
+        self.selectable = self.widget.selectable
+        self.render = self.widget.render
+        self.rows = self.widget.rows
 
-    def __repr__(self):
+
+
+    '''def selectable(self):
+        return False
+    def rows(self, (maxcol,), focus=False):
+        return 1
+        '''
+
+    def update_widget(self):
+        #self.widget = urwid.Text(self.__repr__(), wrap='clip')
+        self.widget.set_text(self.__repr__())
+
+
+    '''def __repr__(self):
         self.ddate = self['messages'][-1]['date']
         self.dsender = u','.join([x['sender'].split()[0].strip('"') for x in self['messages'][-3:]])
         self.dcontained = len(self['messages'])
@@ -36,6 +66,7 @@ class conv_repr(urwid.Text, dict):
         self.disprender = u"%s\t%s\t%i\t%s %s %s" % \
             (self.ddate, self.dsender, self.dcontained, self.dsubject, self.dlabels, self.dpreview)
         return self.disprender
+        '''
 
     '''def set_label(self, label):
         self.label = label
@@ -53,23 +84,23 @@ class conv_repr(urwid.Text, dict):
         newwin(showmessages)
         '''
 
-#lazythread.convContainer = lazythread.convContainer.__new__(convRep)
+lazythread.convContainer = conv_repr
 #lazythread.convContainer = convRep
 
 class myWalker(urwid.SimpleListWalker):
     def get_focus(self):
         if len(self) == 0: return None, None
-        return self.Text(self[self.focus], wrap='clip'), self.focus
+        return self[self.focus].widget, self.focus
 
     def get_next(self, start_from):
         pos = start_from + 1
         if len(self) <= pos: return None, None
-        return urwid.Text(self[pos], wrap='clip'), pos
+        return self[pos].widget, pos
 
     def get_prev(self, start_from):
         pos = start_from - 1
         if pos < 0: return None, None
-        return urwid.Text(self[pos], wrap='clip'), pos
+        return self[pos].widget, pos
 
 class Screen:
     def __init__(self, tui):
@@ -101,12 +132,12 @@ class Screen:
         self.threadview_list = []
         #self.threadwalker = urwid.SimpleListWalker(self.threadview_list)
         #self.threadwalker = urwid.SimpleListWalker([])
-        self.thread.threadList.__new__(myWalker)
-        self.threadwalker = self.thread.threadList
-        self.listbox = urwid.ListBox(self.threadwalker)
+        #self.thread.threadList.__new__(urwid.SimpleListWalker)
+        #self.threadwalker = self.thread.threadList
+        #self.listbox = urwid.ListBox(self.thread)
         #self.listbox = urwid.ListBox(self.threadview_list)
-        #self.threadwalker = urwid.PollingListWalker(self.thread.threadList)
-        #self.listbox = urwid.ListBox(self.threadwalker)
+        self.threadwalker = urwid.PollingListWalker(self.thread.threadList)
+        self.listbox = urwid.ListBox(self.threadwalker)
         self.listbox2 = urwid.ListBox(self.lines2)
         self.input = urwid.Edit()
         self.summarytxt = urwid.Text('inbox, blalkjsdfn %i threads' % len(self.thread), align='left')

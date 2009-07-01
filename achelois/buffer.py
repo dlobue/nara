d45533729bcce70071a90459f727d4d799629713
root <root@megaera.local>
2009-07-01 15:01:34 -0700

    refactored buffer code, middle of writing conversation walker widgets

---
+++ achelois/buffer.py
@@ -0,0 +1,50 @@
+import weakref
+
+class buffer_manager(object):
+    #__metaclass__ = urwid.MetaSignals
+    #signals = ['buffer_update']
+
+    _buffers = weakref.WeakKeyDictionary()
+    #_buffers = WeakKeyOrderedDict()
+    _rbufferobj = None
+    _supported = {}
+    _noremove = []
+
+    @classmethod
+    def register_rootobj(cls, rbufferobj):
+        cls._rbufferobj = rbufferobj
+
+    @classmethod
+    def register_support(cls, data_obj, buffer_obj, noremove=False):
+        cls._supported.setdefault(type(data_obj), buffer_obj)
+        if noremove: cls._noremove.append(buffer_obj)
+
+    @classmethod
+    def register_noremove(cls, buffer_obj):
+        cls._noremove.append(buffer_obj)
+
+    @classmethod
+    def _new_buffer(cls, data):
+        buffer_type = type(data)
+        try: newbuffer = cls._supported[buffer_type]
+        except: raise TypeError("Don't know how to make a buffer for that type of object", buffer_type, cls._supported)
+        else: return cls._buffers.setdefault(data, newbuffer(data))
+
+    @classmethod
+    def get_buffer(cls, data):
+        try: bufferdisp = cls._buffers[data]
+        except: bufferdisp = cls._new_buffer(data)
+        return bufferdisp
+
+    @classmethod
+    def set_buffer(cls, data):
+        bufferdisp = cls.get_buffer(data)
+        cls._rbufferobj(bufferdisp)
+        return bufferdisp
+
+    @classmethod
+    def destroy(cls, data):
+        if cls._buffers[data] not in cls._noremove:
+            try: del cls._buffers[data]
+            except: pass
+
---
+++ achelois/lib/view-util.py
@@ -0,0 +1,18 @@
+
+def _callback(fn):
+    def wrapper(self, *args, **kwargs):
+        r = fn(self, *args, **kwargs)
+        self.parentmethod()
+        return r
+    return wrapper
+
+class callback_list(list):
+    def __init__(self, parentmethod):
+        self.parentmethod = parentmethod
+
+    append = _callback(list.append)
+    extend = _callback(list.extend)
+    sort = _callback(list.sort)
+    __setslice__ = _callback(list.__setslice__)
+    __setitem__ = _callback(list.__setitem__)
+    

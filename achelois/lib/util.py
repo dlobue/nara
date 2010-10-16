590981b8c81efddfb540f6bcbd99abb485eaf0d4
Dominic LoBue <dominic.lobue@gmail.com>
2010-10-16 02:39:42 -0700

    added memoization utility

--- achelois/lib/util.py
+++ achelois/lib/util.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 class MetaSuper(type):
     '''blatent ripoff of Urwid's MetaSuper class
     adds a .__super method I've come to depend on'''
@@ -6,3 +8,21 @@ class MetaSuper(type):
         if hasattr(cls, "_%s__super" % name):
             raise AttributeError, "Class has same name as one of its super classes"
         setattr(cls, "_%s__super" % name, super(cls))
+
+def static_memoize(fctn):
+    @wraps(fctn)
+    def memo(self):
+        cached_mthd_name = '__%s_%s_cache' % (fctn.im_class.__name__,
+                                              fctn.im_func.func_name)
+        if hasattr(self, cached_mthd_name):
+            return getattr(self, cached_mthd_name)
+
+        v = fctn(self)
+        setattr(self, cached_mthd_name, v)
+
+        return v
+    if memo.__doc__:
+        memo.__doc__ = "\n".join([memo.__doc__,"This function is memoized."])
+    return memo
+
+

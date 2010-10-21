dd6d853b361c623d4a902b9876d33d63c8bc237a
Dominic LoBue <dominic.lobue@gmail.com>
2010-10-21 03:14:05 -0700

    rewrote change_scanner to work with maildir format more closely

--- nara/change_scan.py
+++ nara/change_scan.py
@@ -1,16 +1,18 @@
 
 from os.path import join, isdir, getmtime
+from os import listdir
 from datetime import datetime
+import logging
 
 from sqlobject import connectionForURI, sqlhub, SQLObjectNotFound
 from sqlobject.sresults import SelectResults
+from pubsub import pub
 
-from models.fs import Directory, File
+from models.fs import Directory, SubDirectory, File
 from settings import settingsdir
 from overwatch import settings
 
 from tools import null_handler
-import logging
 
 
 nh = null_handler()
@@ -40,6 +42,86 @@ def scan_mail():
                                    parent_dir=None)
     
     return find_missing_and_new([root_maildirs])
+
+
+#TODO: only emails are valid "files"
+#TODO: don't bother recording 'new', 'cur', or 'tmp' maildir subpaths.
+def update_maildir_cache(maildirs, colon=':'):
+    """
+    Scans maildirs looking for new and removed mail.
+    """
+    started_at = datetime.now()
+    imaildirs = iter(maildirs)
+    stash = []
+    substash = []
+
+    while 1:
+        try:
+            mdir = imaildirs.next()
+            if type(mdir) is SelectResults:
+                stash.append(imaildirs)
+                imaildirs = iter(mdir)
+                continue
+        except StopIteration:
+            try:
+                imaildirs = iter(stash.pop())
+                continue
+            except IndexError:
+                break
+        if mdir.has_changed:
+            mdir_contents = mdir.listdir()
+            while 1:
+                try:
+                    item = mdir_contents.next()
+                    itemfp = join(mdir.full_path, item)
+                except StopIteration:
+                    if substash:
+                        mdir_contents = substash.pop()
+                        continue
+                    missing_mail = mdir.files.filter(File.q.last_seen < \
+                                                          started_at)
+                    for mail in missing_mail:
+                        #pub.sendMessage('nara.xindex.delete', docid=mail.uuid)
+                        #XXX: use sqlobject's signalling
+                        mail.destroySelf()
+                    break
+
+                if isdir(itemfp):
+                    if substash:
+                        # if there's something in substash, that means we're in
+                        # a cur or new folder, and there shouldn't be any
+                        # folders under them. skip it
+                        continue
+                    if item in ('cur', 'new'):
+                        # these aren't maildirs, so don't store them as Directory
+                        # records. they are a component of a maildir though, and
+                        # are where the files we want are located.
+                        substash.append(mdir_contents)
+                        mdir_contents = iter(listdir(itemfp))
+                        continue
+
+                    if item == 'tmp':
+                        #files in the tmp folder shouldn't be there long
+                        #don't bother
+                        continue
+                    try:
+                        #item = Directory.selectBy(name=item,
+                        #                          parent_dir=mdir).getOne()
+                        item = mdir.directories.filter(Directory.q.name == item).getOne()
+                    except SQLObjectNotFound:
+                        item = Directory(name=item, parent_dir=mdir,
+                                         last_modified=getmtime(itemfp)-1)
+                    maildirs.append(item)
+                elif substash:
+                    item = item.split(colon)[0]
+                    try:
+                        item = mdir.files.filter(File.q.name == item).getOne()
+                        item.update_seen()
+                    except SQLObjectNotFound:
+                        item = File(name=item, parent_dir=mdir)
+                        #print('add to xapian - %r' % item)
+                        #add to xapian
+
 
 
 def find_missing_and_new(maildirs):
@@ -74,9 +156,8 @@ def find_missing_and_new(maildirs):
                     missing_mail = mdir.files.filter(File.q.last_seen < \
                                                           started_at)
                     for mail in missing_mail:
+                        pub.sendMessage('nara.xindex.delete', docid=mail.uuid)
                         mail.destroySelf()
-                        #print('remove from xapian - %r' % mail)
-                    #missing item - remove from dbs
                     break
 
                 if isdir(itemfp):
@@ -96,8 +177,7 @@ def find_missing_and_new(maildirs):
                         item = File(name=item, parent_dir=mdir)
                         #print('add to xapian - %r' % item)
                         #add to xapian
-
-    return
+                    yield item
 
 
 
--- nara/models/fs.py
+++ nara/models/fs.py
@@ -5,7 +5,77 @@ from uuid import uuid4
 
 from sqlobject import *
 
-class Directory(SQLObject):
+
+class _Record(SQLObject):
+    class sqlmeta:
+        style = Style()
+    name = StringCol(notNone=True)
+    parent_dir = ForeignKey('Directory', cascade=True)
+    full_path_index = DatabaseIndex('name', 'parent_dir', unique=True)
+    last_modified = FloatCol(notNone=True)
+
+    def _get_full_path(self):
+        try:
+            return self.__cached_path
+        except AttributeError:
+            p = [self.name]
+            r = self
+            while 1:
+                r = r.parent_dir
+                if r is None:
+                    self.__cached_path = fpath = join(*p)
+                    return fpath
+                p.insert(0, r.name)
+    
+
+class Directory(_Record):
+    files = SQLMultipleJoin('File', joinColumn='parent_dir')
+    directories = SQLMultipleJoin('Directory', joinColumn='parent_dir')
+
+    def listdir(self):
+        return iter(listdir(self.full_path))
+
+    def _get_has_changed(self):
+        new_mtime = getmtime(self.full_path)
+        if self.last_modified != new_mtime:
+            self.last_modified = new_mtime
+            return new_mtime
+        return False
+    
+    _doc_has_changed = """
+    If there are no changes returns False. If files have 
+    been removed or added, returns the new modified timestamp.
+    """
+
+
+
+class ___SubDirectory(_Record):
+    def listdir(self):
+        return iter(listdir(self.full_path))
+
+    def _get_has_changed(self):
+        new_mtime = getmtime(self.full_path)
+        if self.last_modified != new_mtime:
+            self.last_modified = new_mtime
+            return new_mtime
+        return False
+    
+    _doc_has_changed = """
+    If there are no changes returns False. If files have 
+    been removed or added, returns the new modified timestamp.
+    """
+
+
+
+class ___Directory(SubDirectory):
+    files = SQLMultipleJoin('File', joinColumn='parent_dir')
+    directories = SQLMultipleJoin('Directory', joinColumn='parent_dir')
+    subdirs = SQLMultipleJoin('SubDirectory', joinColumn='parent_dir')
+
+
+
+
+class __Directory(SQLObject):
     class sqlmeta:
         style = Style()
     name = StringCol(notNone=True)
@@ -14,6 +84,7 @@ class Directory(SQLObject):
     last_modified = FloatCol(notNone=True)
     files = SQLMultipleJoin('File', joinColumn='parent_dir')
     directories = SQLMultipleJoin('Directory', joinColumn='parent_dir')
+    subdirs = SQLMultipleJoin('SubDirectory', joinColumn='parent_dir')
 
     def _get_full_path(self):
         try:
@@ -45,8 +116,55 @@ class Directory(SQLObject):
 
 
 
+class __SubDirectory(SQLObject):
+    class sqlmeta:
+        style = Style()
+    name = StringCol(notNone=True)
+    parent_dir = ForeignKey('Directory', cascade=True)
+    full_path_index = DatabaseIndex('name', 'parent_dir', unique=True)
+    last_modified = FloatCol(notNone=True)
 
-class File(SQLObject):
+    def _get_full_path(self):
+        try:
+            return self.__cached_path
+        except AttributeError:
+            p = [self.name]
+            r = self
+            while 1:
+                r = r.parent_dir
+                if r is None:
+                    self.__cached_path = fpath = join(*p)
+                    return fpath
+                p.insert(0, r.name)
+    
+    def listdir(self):
+        return iter(listdir(self.full_path))
+
+    def _get_has_changed(self):
+        new_mtime = getmtime(self.full_path)
+        if self.last_modified != new_mtime:
+            self.last_modified = new_mtime
+            return new_mtime
+        return False
+    
+    _doc_has_changed = """
+    If there are no changes returns False. If files have 
+    been removed or added, returns the new modified timestamp.
+    """
+
+
+
+class File(_Record):
+    last_seen = DateTimeCol(default=DateTimeCol.now)
+    uuid = StringCol(notNone=True, alternateID=True,
+                     default=lambda: uuid4().hex,
+                    alternateMethodName="by_uuid")
+
+    def update_seen(self):
+        self.last_seen = DateTimeCol.now()
+
+
+class __File(SQLObject):
     class sqlmeta:
         style = Style()
     name = StringCol(notNone=True)
@@ -70,6 +188,22 @@ class File(SQLObject):
                 r = r.parent_dir
                 if r is None:
                     self.__cached_path = fpath = join(*p)
+                    return fpath
+                p.insert(0, r.name)
+
+
+    def _get_maildir_path(self):
+        try:
+            return self.__cached_maildir_path
+        except AttributeError:
+            p = [self.name]
+            r = self
+            while 1:
+                r = r.parent_dir
+                if r is None:
+                    self.__cached_maildir_path = fpath = [p.pop(), p.pop(),
+                                                          join(*p)]
+                    fpath.reverse()
                     return fpath
                 p.insert(0, r.name)
 
